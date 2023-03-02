a288717f4d2f67b666410abbab38c0240f0a8177
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-03-02 23:21:54 +0600

    fetch data

--- app.js
+++ app.js
@@ -4,3 +4,41 @@
 // Single data details: https://openapi.programming-hero.com/api/ai/tool/${id}
 
 // Single data Example: https://openapi.programming-hero.com/api/ai/tool/01
+
+const loadData = () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    fetch(url).then(res => res.json()).then(data => appendData(data));
+}
+
+const appendData = (data) => {
+    const container = document.getElementById('container');
+    const mainArray = data.data.tools;
+    console.log(data.data.tools);
+    const cardDiv = document.createElement('div');
+    const list = document.createElement('li');
+    mainArray.forEach(element => {
+        mainArray.slice(0,6);
+        const {name, description, image, published_in, features} = element;
+        container.innerHTML = `
+        <div class="card rounded-lg border p-5  bg-base-100 ">
+                <figure><img src="${image}" class="rounded-xl" /></figure>
+                <div class="mt-3 ">
+                  <h2 class="card-title font-bold">Features</h2>
+                  <ul id="list-container">
+                  ${features[0]}
+                  </ul>
+                  <div class="card-actions justify-between mt-5 items-center">
+                    <div><h2 class="card-title font-bold">${name}</h2>
+                    <div class="flex items-center gap-2 mt-2">
+                    <i class="far fa-calendar-alt"></i>
+                    <p>${published_in}</p>
+                    </div></div>
+                    <button class="font-bold text-xl text-red-500 border-none"><i class="fas fa-arrow-right"></i></button>
+                  </div>
+                </div>
+            </div>
+        `
+    });
+}
+
+loadData()
