066a29d218a2c96bc6a5a937f975613713d6986f
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-03-02 22:20:44 +0600

    header section done

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log(true)
+// API Links
+// All data: https://openapi.programming-hero.com/api/ai/tools
+
+// Single data details: https://openapi.programming-hero.com/api/ai/tool/${id}
+
+// Single data Example: https://openapi.programming-hero.com/api/ai/tool/01
