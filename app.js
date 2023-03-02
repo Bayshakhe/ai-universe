3e2f5d6768c8c14aab37d29c458adf62d4f8421a
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-03-02 23:48:06 +0600

    display all card

--- app.js
+++ app.js
@@ -11,15 +11,17 @@ const loadData = () => {
 }
 
 const appendData = (data) => {
+  const mainArray = data.data.tools;
+  mainArray.forEach(element => {
+        console.log(element)
+        const {name, description, image, published_in, features} = element;
+        
     const container = document.getElementById('container');
-    const mainArray = data.data.tools;
     console.log(data.data.tools);
     const cardDiv = document.createElement('div');
     const list = document.createElement('li');
-    mainArray.forEach(element => {
-        mainArray.slice(0,6);
-        const {name, description, image, published_in, features} = element;
-        container.innerHTML = `
+
+        cardDiv.innerHTML = `
         <div class="card rounded-lg border p-5  bg-base-100 ">
                 <figure><img src="${image}" class="rounded-xl" /></figure>
                 <div class="mt-3 ">
@@ -29,7 +31,7 @@ const appendData = (data) => {
                   </ul>
                   <div class="card-actions justify-between mt-5 items-center">
                     <div><h2 class="card-title font-bold">${name}</h2>
-                    <div class="flex items-center gap-2 mt-2">
+                    <div class="flex items-center gap-2 mt-2 text-zinc-500">
                     <i class="far fa-calendar-alt"></i>
                     <p>${published_in}</p>
                     </div></div>
@@ -38,7 +40,9 @@ const appendData = (data) => {
                 </div>
             </div>
         `
+        container.appendChild(cardDiv);
     });
+    
 }
 
 loadData()
