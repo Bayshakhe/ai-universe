34d908edb11900cc993028797d84c3387a51e559
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-03-03 01:38:25 +0600

    card's information setup dynamically

--- app.js
+++ app.js
@@ -12,22 +12,26 @@ const loadData = () => {
 
 const appendData = (data) => {
   const mainArray = data.data.tools;
+  // mainArray = mainArray.slice(0,6)
   mainArray.forEach(element => {
         console.log(element)
         const {name, description, image, published_in, features} = element;
+        document.getElementById('image').innerHTML = `<img src="${image}" class="rounded-xl" />`
         
-    const container = document.getElementById('container');
-    console.log(data.data.tools);
-    const cardDiv = document.createElement('div');
-    const list = document.createElement('li');
+        const container = document.getElementById('container');
+        const listContainer = document.getElementById('list-container');
+        console.log(data.data.tools);
+        const cardDiv = document.createElement('div');
+        const list = document.createElement('li');
 
+        let count = 0;
         cardDiv.innerHTML = `
         <div class="card rounded-lg border p-5  bg-base-100 ">
                 <figure><img src="${image}" class="rounded-xl" /></figure>
                 <div class="mt-3 ">
                   <h2 class="card-title font-bold">Features</h2>
                   <ul id="list-container">
-                  ${features[0]}
+                  ${features.map(x => x)}
                   </ul>
                   <div class="card-actions justify-between mt-5 items-center">
                     <div><h2 class="card-title font-bold">${name}</h2>
@@ -35,14 +39,15 @@ const appendData = (data) => {
                     <i class="far fa-calendar-alt"></i>
                     <p>${published_in}</p>
                     </div></div>
-                    <button class="font-bold text-xl text-red-500 border-none"><i class="fas fa-arrow-right"></i></button>
+                    <label for="my-modal"  class="font-bold text-xl text-red-500 border-none"><i class="fas fa-arrow-right"></i></label>
+                    
                   </div>
                 </div>
             </div>
         `
+        
         container.appendChild(cardDiv);
     });
     
 }
 
-loadData()
